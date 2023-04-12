26dbf68d88bf3ca9ebf1f12102020b4b0f26f64e
Eduardo Gonzalez <wm.eddie@gmail.com>
2023-04-13 00:46:28 +0900

    Add streaming responses.

--- src/lib/OpenAI.ts
+++ src/lib/OpenAI.ts
@@ -26,3 +26,55 @@ export async function sendMessage(history: Message[]) {
 	const out = await response.json();
 	return out.choices[0].message;
 }
+
+export async function sendMessageAndStream(message: Message, history: Message[], onMessage: (message: string, done: boolean) => void) {
+    const model = PUBLIC_MODEL;
+    const temperature = 0.7;
+
+    const payload = {
+        model: model,
+        temperature: temperature,
+        messages: [...history],
+        stream: true
+    };
+
+    const response = await fetch('/v1/chat/completions', {
+        method: 'POST',
+        body: JSON.stringify(payload),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    const reader = response.body.getReader();
+    const decoder = new TextDecoder('utf-8');
+    let out = '';
+    while (true) {
+        const {done, value} = await reader.read();
+        if (done) {
+            break;
+        }
+        const chunk = decoder.decode(value, {stream: true});
+        out += chunk;
+
+        let eventSeparatorIndex;
+        while ((eventSeparatorIndex = out.indexOf('\n\n')) !== -1) {
+            const data = out.slice(0, eventSeparatorIndex);
+
+            const event = JSON.parse(data.replace(/^data: /, ''));
+
+            out = out.slice(eventSeparatorIndex + 2);
+
+            if (event.choices[0].finish_reason === 'stop') {
+                console.log("End")
+                onMessage("", true); // send end message.
+            } else if (event.choices[0].role === 'assistant') {
+                console.log("Role")
+                onMessage("", false); // send start message.
+            } else {
+                console.log("content: " + event.choices[0].delta.content);
+                onMessage(event.choices[0].delta.content, false);
+            }
+        }
+    }
+}
