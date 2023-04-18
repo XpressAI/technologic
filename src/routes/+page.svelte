79a16dd194ab77c9a2d39339340c4e698909eddd
Eduardo Gonzalez <wm.eddie@gmail.com>
2023-04-18 21:11:52 +0900

    Remove Japanese explainer from main page for now.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 		<h2>Branching Chat GPT</h2>
 	</header>
 	<section class="p-4 gap-2 flex flex-col">
-		<p>Technologic is a chat client that uses RWKV powered by NEC SX-Aurora TSUBASA.</p>
+		<p>Technologic is a chat client that uses ChatGPT or RWKV</p>
 		<p>
 			The unique feature of this chat client is that it allows users to branch off the main
 			conversation at any point to explore different ideas or go on tangents without disrupting the
@@ -27,24 +27,4 @@
 			conversations.
 		</p>
 	</section>
-    <section class="p-4 gap-2 flex flex-col">
-        <p>Technologicは、NEC SX-Aurora TSUBASAによって駆動されるRWKVを使用したチャットクライアントです。</p>
-        <p>
-            このチャットクライアントのユニークな機能は、メインの会話からいつでも分岐して、
-            異なるアイデアを探求したり、本筋から逸れた話題に触れたりできることです。これにより、
-            メインの会話の流れを乱すことなく、さまざまな方向に議論を進めることができます。
-        </p>
-        <p>
-            Technologicのこの分岐機能は、ユーザーエクスペリエンスを向上させ、より探求的で
-            魅力的な会話を可能にするように設計されています。これにより、メインのトピックを汚染する
-            リスクなしに、よりダイナミックでクリエイティブな会話ができます。
-        </p>
-        <p>
-            Technologicは、AIソリューションを開発する専門の企業である
-            <a href="https://www.xpress.ai">Xpress.ai</a>によって作成されました。
-            ユーザーエクスペリエンスを向上させる最先端の技術を提供することに重点を置いて、Xpress.aiの専門家チームは、
-            「Technologic」を開発し、ユーザーがよりダイナミックで魅力的な会話を楽しめるユニークで強力な
-            チャットクライアントを提供しています。
-        </p>
-    </section>
 </main>
