552d86e6b3bef6b377614363c223be7e42d176e5
Paul Dubs <paul.dubs@gmail.com>
2023-04-21 18:52:57 +0200

    Rewrite in-app description to match up with readme

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,25 +12,39 @@
 		<h1 class="flex gap-1">Technologic <IconMessageChatbot size="48" /></h1>
 		<h2>Branching Chat GPT</h2>
 	</header>
-	<section class="p-4 gap-2 flex flex-col">
-		<p>Technologic is a chat client that uses ChatGPT or RWKV</p>
+	<section class="p-4 gap-4 flex flex-col">
 		<p>
-			The unique feature of this chat client is that it allows users to branch off the main
-			conversation at any point to explore different ideas or go on tangents without disrupting the
-			flow of the main conversation.
+			Technologic is a powerful, feature-rich AI Chatbot Client that is designed to work seamlessly with OpenAI's API or any
+			compatible backend. With a user-friendly interface and the ability to organize, modify, and manage your conversations,
+			Technologic brings you a next-level chatting experience with your AI assistant.
 		</p>
-		<p>
-			This branching feature of Technologic is designed to enhance the user experience and enable
-			more exploratory and engaging conversations. It allows for more dynamic and creative
-			conversations without the risk of polluting the main topic.
-		</p>
-		<p>
-			Technologic was created by <a href="https://www.xpress.ai">Xpress.ai</a>, a company that
-			specializes in developing AI solutions. With a focus on delivering cutting-edge technology
-			that enhances the user experience, Xpress.ai's team of experts developed "Technologic" to
-			provide a unique and powerful chat client that enables users to have more dynamic and engaging
-			conversations.
-		</p>
+
+		<div>
+			<h3>Features</h3>
+			<ul>
+				<li><strong>Secure Storage</strong>: Your conversations are stored locally on your computer using your browser's IndexedDB storage.</li>
+				<li><strong>Backend Compatibility</strong>: Works with any backend compatible with OpenAI's API.</li>
+				<li><strong>Bring Your Own API Key</strong>: Easily configure your OpenAI API key or any other compatible backend.</li>
+				<li><strong>Organize Conversations</strong>: Keep your conversations tidy by organizing them into folders.</li>
+				<li><strong>Message Modification</strong>: Edit and modify messages, both sent and received, as needed.</li>
+				<li><strong>Custom Personality</strong>: Support for "System Messages" to give your chatbot a unique personality (if supported by the backend).</li>
+				<li><strong>Fork Conversations</strong>: Easily branch off into different topics without losing the context of previous conversations.</li>
+				<li><strong>Elaborate</strong>: Use the "Go on" feature to prompt the bot to expand on its last message.</li>
+				<li><strong>Merge Messages</strong>: Combine messages to avoid fragmentation or incomplete code blocks.</li>
+				<li><strong>View Raw Message</strong>: Access the raw text of any message with the flip of a switch.</li>
+			</ul>
+		</div>
+
+		<div>
+			<h3>Made by Xpress AI</h3>
+			<p>
+				Technologic was created by <a href="https://www.xpress.ai">Xpress AI</a>, a company that
+				specializes in developing AI solutions. With a focus on delivering cutting-edge technology
+				that enhances the user experience, Xpress.ai's team of experts developed "Technologic" to
+				provide a unique and powerful chat client that enables users to have more dynamic and engaging
+				conversations.
+			</p>
+		</div>
 	</section>
 </main>
 
