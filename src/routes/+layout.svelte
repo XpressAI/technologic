4615c0f293d1e818670ec8c78fad77d2b8cb1123
Paul Dubs <paul.dubs@gmail.com>
2023-06-16 11:23:08 +0200

    Add a sidebar toggle

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,6 +14,9 @@
 
 	import IconMessageChatbot from '@tabler/icons-svelte/dist/svelte/icons/IconMessageChatbot.svelte';
 	import IconSettings from '@tabler/icons-svelte/dist/svelte/icons/IconSettings.svelte';
+	import IconChevronLeft from '@tabler/icons-svelte/dist/svelte/icons/IconChevronLeft.svelte';
+	import IconChevronRight from '@tabler/icons-svelte/dist/svelte/icons/IconChevronRight.svelte';
+
 
 	import '../app.postcss';
 
@@ -29,64 +32,75 @@
 	import { storePopup } from '@skeletonlabs/skeleton';
 	import ConversationGraph from '$lib/components/ConversationGraph.svelte';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
+	let hideSidebar = false;
 </script>
 
 <Modal />
 <AppShell>
 	<svelte:fragment slot="sidebarLeft">
-		<nav
-			class="list-nav p-2 h-full variant-ghost flex flex-col ring-0 border-r border-surface-500 w-56 overflow-y-auto"
-		>
-			<a href="/">
-				<div class="flex flex-col">
-					<div class="flex gap-1 text-2xl font-bold items-center">
-						Technologic <IconMessageChatbot />
-					</div>
-					<span class="text-lg">Branching AI Chat</span>
-				</div>
-			</a>
-			<div class="flex-grow mt-2">
-				<ul>
-					<li><a href="/new">Start new Conversation</a></li>
-					<li><hr /></li>
-					<li><Folder folder={$folderStore} /></li>
-				</ul>
-			</div>
-			<div class="flex place-content-end p-2">
-				<Menu id="settings">
-					<div slot="button">
-						<IconSettings />
-					</div>
-					<div class="card">
-						<ul class="list">
-							<li class="flex gap-2">
-								<LightSwitch /> Dark Mode
-							</li>
-						</ul>
-						<nav class="list-nav">
-							<ul class="list-nav card p-2">
-								<li>
-									<a href="/settings/backends">Backends</a>
-								</li>
-								<li>
-									<a href="/settings/backup">Backup / Restore</a>
-								</li>
-							</ul>
-						</nav>
-					</div>
-				</Menu>
-			</div>
-			<div>
-				<a href="https://www.xpress.ai" class="card flex variant-ghost gap-2 place-content-center">
-					<div class="w-36">
-						<img
-							loading="lazy"
-							src="https://www.xpress.ai/web/image/629-27b48fed/xpress-ai-logo_White_1200px.png"
-						/>
+		<div class="h-full relative min-w-[25px]">
+			<button class="btn btn-sm variant-soft !p-0 absolute right-[1px] top-5 text-surface-600-300-token"
+					title="Toggle Sidebar"
+					on:click={() => hideSidebar = !hideSidebar}>
+				{#if hideSidebar }
+					<IconChevronRight />
+				{:else}
+					<IconChevronLeft />
+				{/if}
+			</button>
+			<nav class="list-nav p-2 h-full variant-ghost flex flex-col ring-0 border-r border-surface-500 w-56 overflow-y-auto" class:hidden={hideSidebar}>
+				<a href="/">
+					<div class="flex flex-col">
+						<div class="flex gap-1 text-2xl font-bold items-center relative">
+							Technologic <IconMessageChatbot />
+						</div>
+						<span class="text-lg">Branching AI Chat</span>
 					</div>
 				</a>
-			</div>
-		</nav>
+				<div class="flex-grow mt-2">
+					<ul>
+						<li><a href="/new">Start new Conversation</a></li>
+						<li><hr /></li>
+						<li><Folder folder={$folderStore} /></li>
+					</ul>
+				</div>
+				<div class="flex place-content-end p-2">
+					<Menu id="settings">
+						<div slot="button">
+							<IconSettings />
+						</div>
+						<div class="card">
+							<ul class="list">
+								<li class="flex gap-2">
+									<LightSwitch /> Dark Mode
+								</li>
+							</ul>
+							<nav class="list-nav">
+								<ul class="list-nav card p-2">
+									<li>
+										<a href="/settings/backends">Backends</a>
+									</li>
+									<li>
+										<a href="/settings/backup">Backup / Restore</a>
+									</li>
+								</ul>
+							</nav>
+						</div>
+					</Menu>
+				</div>
+				<div>
+					<a href="https://www.xpress.ai" class="card flex variant-ghost gap-2 place-content-center">
+						<div class="w-36">
+							<img
+									loading="lazy"
+									src="https://www.xpress.ai/web/image/629-27b48fed/xpress-ai-logo_White_1200px.png"
+							/>
+						</div>
+					</a>
+				</div>
+			</nav>
+		</div>
 	</svelte:fragment>
 
 	<slot />
