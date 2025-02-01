8a7fe11a3492c155a1944a41f6915ec7a0d78b5c
Eduardo Gonzalez <wm.eddie@gmail.com>
2025-02-01 12:09:03 +0900

    Add hardcoded support for the special snowflakes that are o1 and o3. Also escape title text.

--- src/lib/backend/OpenAI.ts
+++ src/lib/backend/OpenAI.ts
@@ -27,69 +27,103 @@ export function createBackend(configuration: BackendConfiguration, model: string
 	}
 
 	async function sendMessage(history: Message[]): Promise<Message> {
-		const payload = {
-			model: model,
-			temperature: temperature,
-			messages: history
-		};
+    if (model.startsWith('o1') || model.startsWith('o3')) {
+      const payload = {
+        model: model,
+        reasoning_effort: 'medium',
+        response_format: {
+          type: 'text'
+        },
+        messages: history
+      };
+      const response = await request(payload);
 
-		const response = await request(payload);
+      const out = await response.json();
+      return out.choices[0].message;
+    } else {
+      const payload = {
+        model: model,
+        temperature: temperature,
+        messages: history
+      };
+      const response = await request(payload);
 
-		const out = await response.json();
-		return out.choices[0].message;
+      const out = await response.json();
+      return out.choices[0].message;
+    }
 	}
 
 	async function sendMessageAndStream(
 		history: Message[],
 		onMessage: (message: string, done: boolean) => Promise<void>
 	) {
-		const payload = {
-			model: model,
-			temperature: temperature,
-			messages: history,
-			stream: true
-		};
+    let payload = null;
 
-		const response = await request(payload);
+    if (model.startsWith('o1') || model.startsWith('o3')) {
+      payload = {
+        model: model,
+        reasoning_effort: 'medium',
+        response_format: {
+          type: 'text'
+        },
+        messages: history,
+        stream: true
+      };
+    } else {
+      payload = {
+        model: model,
+        temperature: temperature,
+        messages: history,
+        stream: true
+      };
+    }
 
-		const reader = response.body?.getReader();
-		if (!reader) {
-			throw new Error('Could not get reader from response body');
-		}
+    const response = await request(payload);
 
-		const decoder = new TextDecoder('utf-8');
-		let out = '';
-		while (true) {
-			const { done, value } = await reader.read();
-			if (done) {
-				break;
-			}
-			const chunk = decoder.decode(value, { stream: true });
-			out += chunk;
+    const reader = response.body?.getReader();
+    if (!reader) {
+      throw new Error('Could not get reader from response body');
+    }
 
-			let eventSeparatorIndex;
-			while ((eventSeparatorIndex = out.indexOf('\n\n')) !== -1) {
-				const data = out.slice(0, eventSeparatorIndex);
+    const decoder = new TextDecoder('utf-8');
+    let out = '';
+    while (true) {
+      const { done, value } = await reader.read();
+      if (done) {
+        break;
+      }
+      const chunk = decoder.decode(value, { stream: true });
+      out += chunk;
 
-				if (data.match(/^data: \[DONE\]/)) {
-					await onMessage('', true); // send end message.
-					return;
-				}
+      let eventSeparatorIndex;
+      while ((eventSeparatorIndex = out.indexOf('\n\n')) !== -1) {
+        const data = out.slice(0, eventSeparatorIndex);
 
-				const event = JSON.parse(data.replace(/^data: /, ''));
+        if (data.match(/^data: \[DONE\]/)) {
+          await onMessage('', true); // send end message.
+          return;
+        }
 
-				out = out.slice(eventSeparatorIndex + 2);
+        const event = JSON.parse(data.replace(/^data: /, ''));
 
-				if (event.choices[0].finish_reason === 'stop') {
-					await onMessage('', true); // send end message.
-				} else if (event.choices[0].role === 'assistant') {
-					await onMessage('', false); // send start message.
-				} else {
-					await onMessage(event.choices[0].delta.content, false);
-				}
-			}
-		}
+        out = out.slice(eventSeparatorIndex + 2);
+
+        if (event.choices[0].finish_reason === 'stop') {
+          await onMessage('', true); // send end message.
+        } else if (event.choices[0].role === 'assistant') {
+          await onMessage('', false); // send start message.
+        } else {
+          await onMessage(event.choices[0].delta.content, false);
+        }
+      }
+    }
 	}
+
+  function escapeHtml(unsafeText: string) {
+    const div = document.createElement('div');
+    div.textContent = unsafeText;
+    return div.innerHTML;
+  }
 
 	async function renameConversationWithSummary(currentConversation: ConversationStore) {
 		const summarizeMessage = 'Using the same language, in at most 3 words summarize the conversation between assistant and user.'
@@ -112,7 +146,7 @@ export function createBackend(configuration: BackendConfiguration, model: string
 
 		const response = await sendMessage([...filteredHistory, systemMessage, userMessage]);
 
-		const newTitle = response.content;
+		const newTitle = escapeHtml(response.content);
 		if (newTitle) {
 			await currentConversation.rename(newTitle);
 		}
